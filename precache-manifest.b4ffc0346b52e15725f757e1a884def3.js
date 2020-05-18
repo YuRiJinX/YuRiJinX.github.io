4983c49dbc206eef4b23c2b240f3b510606823eb
YuRiJin <875830716@qq.com>
2020-05-19 00:01:44 +0800

    Updates

---
+++ precache-manifest.b4ffc0346b52e15725f757e1a884def3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1a6558823215cc9d09922a5fbb49fbc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6955e782dbaf4003216d",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d46359d8e7eb8c2edf4b",
+    "url": "/static/js/2.0d91727e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0d91727e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6955e782dbaf4003216d",
+    "url": "/static/js/main.9ce90b1b.chunk.js"
+  },
+  {
+    "revision": "921ac687539ca31687de",
+    "url": "/static/js/runtime-main.7c2b1bab.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
